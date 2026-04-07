7b98c6525d8150a691bae5df29dbdd7fc98ce3e9
Amer Alimanović <22625371+amer8@users.noreply.github.com>
2026-04-07 18:28:33 +0200

    Mitigate SSRF security warning

--- services/pokeapi.ts
+++ services/pokeapi.ts
@@ -1,6 +1,28 @@
 import axios from "axios";
 import { findPokemon, savePokemon } from "./pokemons";
 
+const POKEAPI_ORIGIN = "https://pokeapi.co";
+const POKEAPI_BASE_PATH = "/api/v2";
+
+const pokeApi = axios.create({
+  baseURL: `${POKEAPI_ORIGIN}${POKEAPI_BASE_PATH}`,
+});
+
+function extractAbilityId(abilityUrl: string) {
+  const url = new URL(abilityUrl);
+
+  if (url.origin !== POKEAPI_ORIGIN) {
+    throw new Error(`Unexpected ability origin: ${url.origin}`);
+  }
+
+  const abilityPath = url.pathname.match(/^\/api\/v2\/ability\/(\d+)\/?$/);
+  if (!abilityPath) {
+    throw new Error(`Unexpected ability path: ${url.pathname}`);
+  }
+
+  return abilityPath[1];
+}
+
 export async function fetchRandomPokemon() {
   const maxPokemon = 1010;
   let pokemon;
@@ -13,9 +35,7 @@ export async function fetchRandomPokemon() {
       return pokemon;
     }
 
-    const res = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${randomId}`
-    );
+    const res = await pokeApi.get(`/pokemon/${randomId}`);
     if (res.data.base_experience) {
       pokemon = res.data;
     }
@@ -25,7 +45,8 @@ export async function fetchRandomPokemon() {
   for (const ab of pokemon.abilities) {
     if (ab.is_hidden) continue;
 
-    const { data } = await axios.get(ab.ability.url);
+    const abilityId = extractAbilityId(ab.ability.url);
+    const { data } = await pokeApi.get(`/ability/${abilityId}`);
     abilities.push({ ...ab, full: data });
   }
 
