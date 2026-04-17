ae7f4e569f0ef563ff9abe3fcc4629da8e39ac98
Amer Alimanović <22625371+amer8@users.noreply.github.com>
2026-04-17 11:19:04 +0200

    Card flip animation

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -10,55 +10,77 @@ interface Props {
 }
 
 const PokemonCard = ({ pokemon }: Props) => {
-  return pokemon ? (
-    <article className={styles.pokemonCard}>
-      <div className={styles.pokemonCardInner}>
-        <div className={styles.pokemonCardHeader}>
-          <div className={styles.pokemonCardTitle}>{pokemon.name}</div>
-          <ExpTag baseExp={pokemon.base_experience} />
-        </div>
-        <div className={styles.pokemonCardMedia}>
-          <PokemonImg
-            size="96px"
-            src={pokemon.sprites.front_default}
-            pokemonName={pokemon.name}
-          />
-        </div>
-        <div className={styles.pokemonCardMeta}>
-          <div className={styles.pokemonCardBlock}>
-            <div className={styles.wrapRow}>
-              {pokemon.types.map((type) => (
-                <TypeBadge
-                  amount={1}
-                  key={type.type.name}
-                  type={type.type.name.replaceAll("-", " ")}
-                />
-              ))}
-            </div>
-          </div>
+  const isFilled = Boolean(pokemon);
 
-          <div className={styles.pokemonCardBlock}>
-            <div className={styles.pokemonCardSection}>Abilities</div>
-            <div className={styles.wrapRow}>
-              {pokemon.abilities.map((ability) => (
-                <AbilityTag ability={ability.full} key={ability.ability.name} />
-              ))}
+  return (
+    <article className={styles.pokemonCardShell}>
+      <div
+        className={`${styles.pokemonCardFlip} ${
+          isFilled ? styles.pokemonCardFlipRevealed : ""
+        }`}
+      >
+        <div
+          className={`${styles.pokemonCardFace} ${styles.pokemonCardFaceBack}`}
+        >
+          <div className={styles.pokemonCardEmpty}>
+            <div className={styles.pokemonCardEmptyInner}>
+              {/* eslint-disable-next-line @next/next/no-img-element -- local static icon is simpler here than next/image */}
+              <img
+                alt="Pokeball logo"
+                className={styles.pokemonCardEmptyLogo}
+                src="Poke_Ball_icon.svg"
+              />
             </div>
           </div>
+        </div>
+        <div
+          className={`${styles.pokemonCardFace} ${styles.pokemonCardFaceFront}`}
+        >
+          {pokemon ? (
+            <div className={styles.pokemonCard}>
+              <div className={styles.pokemonCardInner}>
+                <div className={styles.pokemonCardHeader}>
+                  <div className={styles.pokemonCardTitle}>{pokemon.name}</div>
+                  <ExpTag baseExp={pokemon.base_experience} />
+                </div>
+                <div className={styles.pokemonCardMedia}>
+                  <PokemonImg
+                    size="96px"
+                    src={pokemon.sprites.front_default}
+                    pokemonName={pokemon.name}
+                  />
+                </div>
+                <div className={styles.pokemonCardMeta}>
+                  <div className={styles.pokemonCardBlock}>
+                    <div className={styles.wrapRow}>
+                      {pokemon.types.map((type) => (
+                        <TypeBadge
+                          amount={1}
+                          key={type.type.name}
+                          type={type.type.name.replaceAll("-", " ")}
+                        />
+                      ))}
+                    </div>
+                  </div>
+
+                  <div className={styles.pokemonCardBlock}>
+                    <div className={styles.pokemonCardSection}>Abilities</div>
+                    <div className={styles.wrapRow}>
+                      {pokemon.abilities.map((ability) => (
+                        <AbilityTag
+                          ability={ability.full}
+                          key={ability.ability.name}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     </article>
-  ) : (
-    <div className={styles.pokemonCardEmpty}>
-      <div className={styles.pokemonCardEmptyInner}>
-        {/* eslint-disable-next-line @next/next/no-img-element -- local static icon is simpler here than next/image */}
-        <img
-          alt="Pokeball logo"
-          className={styles.pokemonCardEmptyLogo}
-          src="Poke_Ball_icon.svg"
-        />
-      </div>
-    </div>
   );
 };
 
